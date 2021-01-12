47fd625196a234c9842e1e009f581a0ae2d63aea
Maxim <maxim.gorbatyuk@outlook.com>
2021-01-12 20:31:59 +0600

    Fix

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Developer hiring test
+
+## How to start the app:
+
+1. Execute `run.ps1` script
+2. Open SPA via [http://localhost:8080](http://localhost:8080)
--- run.ps1
+++ run.ps1
@@ -1,3 +1,10 @@
+# $env:USERPROFILE works only in windows and means %USERPROFILE%
+# https://devblogs.microsoft.com/scripting/powertip-use-powershell-to-find-user-profile-path/
+
+# https://docs.microsoft.com/en-us/aspnet/core/security/docker-compose-https?view=aspnetcore-3.1
+dotnet dev-certs https -ep $env:USERPROFILE\.aspnet\https\aspnetapp.pfx -p password
+dotnet dev-certs https --trust
+
 docker-compose -f "docker-compose.yml" stop
 docker-compose -f "docker-compose.yml" rm --force
 docker-compose -f "docker-compose.yml" up --build mongo backend frontend
