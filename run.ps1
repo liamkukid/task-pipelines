10cf92865ea1dd435cc8648745b680f79115698e
Maxim <maxim.gorbatyuk@outlook.com>
2021-01-12 21:03:08 +0600

    Fix

--- run.ps1
+++ run.ps1
@@ -5,6 +5,8 @@
 dotnet dev-certs https -ep $env:USERPROFILE\.aspnet\https\aspnetapp.pfx -p password
 dotnet dev-certs https --trust
 
+Start-Process "http://localhost:4200"
+
 docker-compose -f "docker-compose.yml" stop
 docker-compose -f "docker-compose.yml" rm --force
 docker-compose -f "docker-compose.yml" up --build mongo backend frontend
